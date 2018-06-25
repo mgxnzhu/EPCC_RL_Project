e97796d128ea37a6bf44a95774ccbb1ce84ec159
mgxnzhu <mgxzhu@gmail.com>
2018-06-25 23:11:37 +0100

    bug fixed

--- scripts/q_learning_quadratic.py
+++ scripts/q_learning_quadratic.py
@@ -12,42 +12,45 @@ dim_obs = env.get_observation_space_size()
 dim_act = env.get_action_space_size()
 
 # Set the range of action values
-action_low = -1
-action_high = 1
+action_low = -0.1
+action_high = 0.1
 
 # Set hyperparameters
-discount = 0.01
-epsilon = 0.1
+discount = 0.0001
+epsilon = 0.9
 episode = 2000
 
-
 class qfunction:
+    # random initialization
     def __init__(self, dim_obs, dim_act, rng=None):
         if rng is None:
             rng = np.random.RandomState(DEFAULT_SEED)
         self.rng = rng
-        
-        self.dim_obs = dim_obs
-        self.dim_act = dim_act
-        self.obs_coeff = rng.uniform(-1, 1, dim_obs)
-        self.act_coeff = rng.uniform(-1, 1, dim_act)
-        self.dim = dim_obs + dim_act
-        
-    def __call__(self, obs, act):
-        obs_term = obs * self.obs_coeff
-        act_term = act * self.act_coeff
-        res = np.sum(obs_term) + np.sum(act_term)
-        return res
-    
-    def update(self, coeff):
-        self.obs_coeff = coeff[:self.dim_obs]
-        self.act_coeff = coeff[self.dim_obs:]
+        self.dim = (dim_obs + dim_act + 1) * (dim_obs + dim_act) // 2
+        self.coeff = rng.uniform(-1, 1, self.dim)
 
+    def __call__(self, obs, act):
+        con_vec = np.concatenate((obs,act))
+        quad_vec = quadartic(con_vec)
+        res = np.sum(quad_vec*self.coeff)
+        return res
+
+    def update(self, coeff):
+        self.coeff = coeff
+        
+def quadartic(vec):
+    # covert (x1, x2, x3, ...) to (x1^2, x1x2, x1x3, ..., x2^2, x2x3, ...)
+    if not isinstance(vec, np.ndarray):
+        vec = np.array(vec)
+    res = []
+    for i, x in enumerate(vec):
+        res = np.concatenate((res, x*vec[i:]))
+    return res
 
 # Initialize Q function
 qf = qfunction(dim_obs, dim_act)
 model = LinearRegression(fit_intercept=False)
-# Initialize the dataset
+# Initialize the dataset:(xdata, ydata)
 xdata = np.zeros((qf.dim,))
 ydata = np.zeros((1,))
 
@@ -81,7 +84,7 @@ for i in range(episode):
         res = minimize(action_func, action0, method='SLSQP', bounds=bnds)
         max_q = -res.fun
         # {s, a} and [r + gamma * max_a` Q(s`, a`)]
-        xx = np.concatenate((state, action))
+        xx = quadartic(np.concatenate((state, action)))
         yy = np.array(reward + discount * max_q)
         # put the data point into dataset
         xdata = np.vstack((xdata, xx))
@@ -93,5 +96,3 @@ for i in range(episode):
 
         # Update state
         state = state_
-
-np.save("model.npy", model.coef_)
